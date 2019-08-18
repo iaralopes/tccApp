750374d2a8452380d323afdab4827aa1ee8142db
Iara Lopes <iara.lopes@maxmilhas.com.br>
2019-08-18 20:15:22 -0300

    Criando a data class Teacher para armazenar o id e nome

--- app/src/main/java/com/example/tccapp/TeachersViewModel.kt
+++ app/src/main/java/com/example/tccapp/TeachersViewModel.kt
@@ -5,16 +5,16 @@ import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
 class TeachersViewModel @Inject constructor() : ViewModel() {
-    val teachers = ObservableField<List<String>>(emptyList())
+    val teachers = ObservableField<List<Teacher>>(emptyList())
 
     init {
         teachers.set(
             listOf(
-                "Lesandro",
-                "Guta",
-                "Rommel",
-                "Marcelo",
-                "Eveline"
+                Teacher(1, "Lesandro"),
+                Teacher(2, "Guta"),
+                Teacher(3, "Rommel"),
+                Teacher(4, "Marcelo"),
+                Teacher(5, "Eveline")
             )
         )
     }
