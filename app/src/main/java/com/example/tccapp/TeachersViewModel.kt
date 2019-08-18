9fa981fd93398afa5aafa6fb47d572558bd6d454
Iara Lopes <iara.lopes@maxmilhas.com.br>
2019-08-18 20:28:59 -0300

    Melhorando o código.

--- app/src/main/java/com/example/tccapp/TeachersViewModel.kt
+++ app/src/main/java/com/example/tccapp/TeachersViewModel.kt
@@ -13,27 +13,39 @@ class TeachersViewModel @Inject constructor() : ViewModel() {
     val teachers = ObservableField<List<TeacherViewEntity>>(emptyList())
 
     init {
-        teachers.set(
-            listOf(
-                TeacherViewEntity(1, "Lesandro"),
-                TeacherViewEntity(2, "Guta"),
-                TeacherViewEntity(3, "Rommel"),
-                TeacherViewEntity(4, "Marcelo"),
-                TeacherViewEntity(5, "Eveline")
-            )
-        )
+//        teachers.set(
+//            listOf(
+//                TeacherViewEntity(1, "Lesandro"),
+//                TeacherViewEntity(2, "Guta"),
+//                TeacherViewEntity(3, "Rommel"),
+//                TeacherViewEntity(4, "Marcelo"),
+//                TeacherViewEntity(5, "Eveline")
+//            )
+//        )
 
         val call = RetrofitInitializer().teachersService().teachers()
-        call.enqueue(object: retrofit2.Callback<List<Teacher>?> {
-            override fun onResponse(call: Call<List<Teacher>?>?,
-                                    response: Response<List<Teacher>?>?) {
+        call.enqueue(object : retrofit2.Callback<List<Teacher>?> {
+            override fun onResponse(
+                call: Call<List<Teacher>?>?,
+                response: Response<List<Teacher>?>?
+            ) {
                 response?.body()?.let {
-                    teachers.set(it.map(Teacher::name))
+                    teachers.set(
+                        it.map {
+                            TeacherViewEntity(
+                                it.id,
+                                it.name
+                            )
+                        }
+                    )
                 }
 
             }
-            override fun onFailure(call: Call<List<Teacher>?>?,
-                                   t: Throwable?) {
+
+            override fun onFailure(
+                call: Call<List<Teacher>?>?,
+                t: Throwable?
+            ) {
                 Log.e("onFailure error", t?.message)
             }
         })
--- app/src/main/java/com/example/tccapp/service/Teacher.kt
+++ app/src/main/java/com/example/tccapp/service/Teacher.kt
@@ -1,5 +1,6 @@
 package com.example.tccapp.service
 
 data class Teacher(
+    val id: Int,
     val name: String
 )
