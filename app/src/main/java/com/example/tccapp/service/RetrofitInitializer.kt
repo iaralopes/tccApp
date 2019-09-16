1acf9f8b7d46263ba9b5a0ac1f1eccba836a1a9b
Iara Lopes <iara.lopes@maxmilhas.com.br>
2019-09-15 21:14:01 -0300

    Atualizando o projeto

--- app/src/main/java/com/example/tccapp/service/RetrofitInitializer.kt
+++ app/src/main/java/com/example/tccapp/service/RetrofitInitializer.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RetrofitInitializer {
 
     private val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.tcc.com.br/")
+            .baseUrl("https://mighty-cove-74154.herokuapp.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
